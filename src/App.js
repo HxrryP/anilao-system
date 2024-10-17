a167a62a1a500fcbb9a78fdcdcf3bf2797dbd885
Harry <harrypacss@gmail.com>
2024-10-17 20:16:52 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Mahal <code>I love youuu</code> Take care always mahal ha.
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Kiss bi!
         </a>
       </header>
     </div>
