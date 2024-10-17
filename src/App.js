4bf4eef172e09650cb81457f03d8a8f3eb41751b
Harry <harrypacss@gmail.com>
2024-10-17 20:23:37 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://iconduck.com/emojis/301423/face-blowing-a-kiss"
           target="_blank"
           rel="noopener noreferrer"
         >
